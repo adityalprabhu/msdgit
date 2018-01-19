fcb4ba4fd317ea6c1392c671918dee82ea9ad1cc
Aditya Prabhu <adityalprabhu@gmail.com>
2018-01-19 14:26:58 -0500

    satisfying requirement change two

--- HW1/test/printrange/CheckRange.java
+++ HW1/test/printrange/CheckRange.java
@@ -20,7 +20,7 @@ public class CheckRange {
 
 	@Test
 	public void testAllNumbersInRange(){
-		List<Integer> list1 = Arrays.asList(2,3,4,5,6,7,8,9);
+		List<Integer> list1 = Arrays.asList(3,5,7,9);
 		Range ob1 = new FindRange();
 		assertEquals(list1, ob1.printAllNumbersInRange(1,10));
 		}
